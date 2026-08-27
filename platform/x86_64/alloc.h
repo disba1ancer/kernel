35ee0f96a9d55d4ceeeacfe9ab75f708371bf6bd
disba1ancer <antonbobachev@gmail.com>
2026-08-27 22:50:44 +0300

    Allocator interface

--- platform/x86_64/alloc.h
+++ platform/x86_64/alloc.h
@@ -1,3 +1,8 @@
+/**
+ * @file alloc.h
+ * @brief Allocation functions declarations
+ */
+
 #ifndef ALLOC_H
 #define ALLOC_H
 
@@ -18,13 +23,57 @@ struct VirtualRange {
         void* start;
         int error;
     };
-    std::size_t size;
+    std::ptrdiff_t size;
 };
 
 struct PageMM {
     PhysicalRange (*PAlloc)(PageMM* mm, void* helperPage, std::size_t size);
-    VirtualRange (*VAlloc)(PageMM* mm, void* page, std::size_t size, int flags, std::uint64_t pArgs);
 };
+
+enum FlagsGeneric {
+    Reserve = 1,
+
+    Alloc = 2,
+    Map = 4,
+    AllocMask = 6,
+
+    Write = 8,
+    Execute = 16,
+    System = 32,
+
+    NoCache = 64,
+    WriteCombine = 128,
+    CacheMask = 192,
+
+    ModeMask = 248,
+};
+
+/**
+ * @brief `valloc`
+ * Allocates at least `size` bytes of memory or
+ * maps specified physical address range.
+ *
+ * @param `size`
+ * Size of requested region. For memory allocations,
+ * this argument is automatically aligned up to
+ * a multiple of the page size.
+ *
+ * @param `flags`
+ * Memory and allocation flags.
+ *
+ * @param `psyaddr`
+ * If is not 0, defines maximum physical address of
+ * contiguous allocation end.
+ *
+ * @return
+ * On error, returns zero size virtual range, where
+ * `error` member stores error code. On success, returns
+ * start virtual address of allocated region in `start`
+ * member and size of the region in `size`.
+ */
+auto valloc(std::ptrdiff_t size, int flags, std::uint64_t phyaddr) -> VirtualRange;
+void vmod(void* addr, std::ptrdiff_t size, int flags);
+void vfree(void* addr, std::ptrdiff_t size, int flags);
 
 } // namespace kernel::tgtspec
 
